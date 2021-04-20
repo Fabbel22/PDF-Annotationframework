3d68ba3839a046cb42d787426961d6f62ae8c33d
Fabbel22 <Fabian.Hallmann@gmx.de>
2021-04-20 19:37:33 +0200

    wip

---
+++ syncfusion-pdfviewer-test3/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService,
+  ToolbarService, NavigationService, TextSearchService, AnnotationService, TextSelectionService, PrintService
+} from '@syncfusion/ej2-angular-pdfviewer';
+
+@Component({
+  selector: 'app-container',
+  // specifies the template string for the PDF Viewer component
+  templateUrl: 'app.component.html',
+  providers: [LinkAnnotationService, BookmarkViewService, MagnificationService,
+    ThumbnailViewService, ToolbarService, NavigationService, AnnotationService, TextSearchService, TextSelectionService, PrintService]
+})
+export class AppComponent implements OnInit {
+  public service = 'https://ej2services.syncfusion.com/production/web-services/api/pdfviewer';
+  public document = 'PDF_Succinctly.pdf';
+  ngOnInit(): void {
+  }
+}
